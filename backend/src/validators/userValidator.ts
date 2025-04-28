a343986021311f62b0c97e4e04344b4f09aec44e
Raj Dutta <rd2249619@gmail.com>
2025-04-28 14:32:42 +0600

    add validator using experss-validator in backend

---
+++ backend/src/validators/userValidator.ts
@@ -0,0 +1,26 @@
+import { body } from 'express-validator';
+
+export const registerValidation = [
+  body('username')
+    .trim()
+    .notEmpty()
+    .withMessage('Username is required.')
+    .isLength({ min: 3, max: 15 })
+    .withMessage(
+      'Username must be at least 3 characters long and at most 15 characters long.'
+    ),
+
+  body('email').isEmail().withMessage('Email is invalid.').normalizeEmail(),
+
+  body('password')
+    .notEmpty()
+    .withMessage('Password is required.')
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters long.'),
+];
+
+export const loginValidation = [
+  body('email').isEmail().withMessage('Email is invalid.').normalizeEmail(),
+
+  body('password').notEmpty().withMessage('Password is required.'),
+];
