b30699668adda744c7abd55fad97b52ad0542f8b
Raj Dutta <rd2249619@gmail.com>
2025-04-28 14:28:29 +0600

    add user controller to backend

---
+++ backend/src/controllers/users.controllers.ts
@@ -0,0 +1,103 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from 'express';
+import UserService from '../services/users.services';
+import {
+  loginValidation,
+  registerValidation,
+} from '../validators/userValidator';
+import { validationResult } from 'express-validator';
+import chalk from 'chalk';
+import { getIO } from '../routes/socket.route';
+
+export default abstract class UserController {
+  static registerValidation = registerValidation;
+  static loginValidation = loginValidation;
+
+  static loginUser = async (req: Request, res: Response) => {
+    try {
+      console.log(chalk.bgGreen.white('Login'), 'user login');
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+
+        return;
+      }
+
+      const user = await UserService.loginUser(req.body);
+
+      res
+        .cookie('refreshToken', user.token.refresh_token, {
+          httpOnly: true,
+          secure: true,
+          maxAge: 30 * 24 * 60 * 60 * 1000,
+        })
+        .status(200)
+        .json(user);
+    } catch (err: any) {
+      res
+        .status(err?.statusCode || 500)
+        .json({
+          error: true,
+          message: err?.message || 'Internal Server Error',
+        });
+    }
+  };
+
+  static getUserList = async (req: Request, res: Response) => {
+    try {
+      const email = req?.user?.email;
+
+      const users = await UserService.getUserList(email || '');
+
+      res.status(200).json(users);
+    } catch (err: any) {
+      res
+        .status(err?.statusCode || 500)
+        .json({
+          error: true,
+          message: err?.message || 'Internal Server Error',
+        });
+    }
+  };
+
+  static createUser = async (req: Request, res: Response) => {
+    try {
+      console.log(chalk.bgGreen.white('registration'), 'user registration');
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        res
+          .status(400)
+          .json({
+            data: { error: true, message: errors.array()[0].msg },
+            status: 400,
+          });
+
+        return;
+      }
+
+      const user = await UserService.createUser(req.body);
+      const io = getIO();
+      const userList = await UserService.getUserList();
+
+      io.emit('all_users', userList);
+
+      res
+        .cookie('refreshToken', user.token.refresh_token, {
+          httpOnly: true,
+          secure: true,
+          maxAge: 30 * 24 * 60 * 60 * 1000,
+        })
+        .status(201)
+        .json(user);
+    } catch (err: any) {
+      res
+        .status(err?.statusCode || 500)
+        .json({
+          error: true,
+          message: err?.message || 'Internal Server Error',
+        });
+    }
+  };
+}
