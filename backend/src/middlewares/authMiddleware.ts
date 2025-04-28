19b79362eb223f15716c090a61818317b59c75d2
Raj Dutta <rd2249619@gmail.com>
2025-04-28 14:29:26 +0600

    add middleware to auth in backend

---
+++ backend/src/middlewares/authMiddleware.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+import { verifyToken, extractToken } from '../utils/jwt'; // Adjust the path as needed
+
+export const authenticate = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  try {
+    const token = extractToken(req);
+
+    if (!token) {
+      res
+        .status(401)
+        .json({ error: true, message: 'No token provided, please log in' });
+
+      return;
+    }
+
+    const decoded = verifyToken(token);
+    req.user = decoded;
+
+    next();
+  } catch {
+    res.status(401).json({ error: true, message: 'Invalid or expired token' });
+
+    return;
+  }
+};
