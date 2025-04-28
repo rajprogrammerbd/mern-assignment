ad8457005f4c183a5ecca6b5f07acf665ddd2329
Raj Dutta <rd2249619@gmail.com>
2025-04-28 14:30:08 +0600

    add user service to backend

---
+++ backend/src/services/users.services.ts
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  ICreateUserProperties,
+  ILoginUserProperties,
+  ILoginUserResponse,
+  IUser,
+} from '../types';
+import { ApiError } from '../utils/ApiError';
+import prisma from '../utils/db';
+import { AuthService } from './auth.service';
+
+export default abstract class UserService {
+  static createUser = async ({
+    username,
+    email,
+    password,
+  }: ICreateUserProperties): Promise<ILoginUserResponse> => {
+    const newUser = AuthService.register(username, email, password);
+
+    return newUser;
+  };
+
+  static getUserList = async (userEmail?: string): Promise<IUser[]> => {
+    try {
+      const users = await prisma.user.findMany({
+        where: {
+          email: {
+            not: userEmail ? userEmail : '',
+          },
+        },
+        select: {
+          id: true,
+          username: true,
+          email: true,
+        },
+      });
+
+      if (users.length === 0) {
+        return [];
+      }
+
+      return users;
+    } catch (error: any) {
+      throw new ApiError(error.statusCode, error);
+    }
+  };
+
+  static loginUser = async ({
+    email,
+    password,
+  }: ILoginUserProperties): Promise<ILoginUserResponse> => {
+    const user = AuthService.login(email, password);
+
+    return user;
+  };
+}
