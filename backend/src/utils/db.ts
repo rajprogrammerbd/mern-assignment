2164336292f6b118489ae29d937758dfed04084c
Raj Dutta <rd2249619@gmail.com>
2025-04-28 14:31:25 +0600

    add db config to backend

---
+++ backend/src/utils/db.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+});
+
+prisma
+  .$connect()
+  .then(() => console.log('Connected to PostgreSQL via Prisma'))
+  .catch((err: Error) => {
+    console.error('Connection error:', err);
+    process.exit(1);
+  });
+
+// Graceful shutdown
+const shutdown = async () => {
+  await prisma.$disconnect();
+  console.log('Prisma client disconnected');
+  process.exit(0);
+};
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
+
+export default prisma;
