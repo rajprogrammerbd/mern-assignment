4d88ac3ec26f1d15c9c3456c110f36037e983d39
Raj Dutta <rd2249619@gmail.com>
2025-04-30 09:44:24 +0600

    add docs to task route

---
+++ backend/src/routes/task.route.ts
@@ -0,0 +1,106 @@
+import { Router } from 'express';
+import TaskController from '../controllers/task.controller';
+import { authenticate } from '../middlewares/authMiddleware';
+import { param } from 'express-validator';
+
+const router = Router();
+
+// Authenticated Routes
+
+// Add a task
+/**
+ * @swagger
+ * tags:
+ *   name: Tasks
+ *   description: Task management endpoints
+ */
+
+/**
+ * @swagger
+ * /api/tasks:
+ *   post:
+ *     summary: Create a new task
+ *     tags: [Tasks]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/TaskCreate'
+ *     responses:
+ *       201:
+ *         description: Task created successfully
+ *       400:
+ *         description: Validation error
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
+router.post(
+  '/add',
+  TaskController.createTaskValidation,
+  authenticate,
+  TaskController.createTask
+);
+
+// Get all tasks
+/**
+ * @swagger
+ * /api/tasks:
+ *   get:
+ *     summary: Get all tasks
+ *     tags: [Tasks]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of tasks retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Task'
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
+router.get('/', authenticate, TaskController.getAllTasks);
+
+// Delete a task
+/**
+ * @swagger
+ * /api/tasks/{taskId}:
+ *   delete:
+ *     summary: Delete a task
+ *     tags: [Tasks]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: taskId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID of the task to delete
+ *     responses:
+ *       201:
+ *         description: Task deleted successfully
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Task not found
+ *       500:
+ *         description: Internal server error
+ */
+router.delete(
+  '/:taskId',
+  [param('taskId').isMongoId().withMessage('Invalid task ID'), authenticate],
+  TaskController.deleteTask
+);
+
+export default router;
