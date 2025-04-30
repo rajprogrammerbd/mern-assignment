b76464cbbd5615160f773bac61c28380d83c3016
Raj Dutta <rd2249619@gmail.com>
2025-04-30 09:49:07 +0600

    updated swaggerOptions to backend

--- backend/src/utils/swaggerOptions.ts
+++ backend/src/utils/swaggerOptions.ts
@@ -1,19 +1,86 @@
-const options = {
+import { type Options } from 'swagger-jsdoc';
+
+const swaggerOptions: Options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Express API with Swagger',
+      title: 'Your API',
       version: '1.0.0',
-      description:
-        'A simple API documentation using swagger-ui-express and swagger-jsdoc',
     },
-    servers: [
-      {
-        url: 'http://localhost:5000',
+    components: {
+      schemas: {
+        UserRegistration: {
+          type: 'object',
+          required: ['username', 'email', 'password'],
+          properties: {
+            username: {
+              type: 'string',
+              minLength: 3,
+              maxLength: 15,
+              example: 'john_doe'
+            },
+            email: {
+              type: 'string',
+              format: 'email',
+              example: 'user@example.com'
+            },
+            password: {
+              type: 'string',
+              minLength: 6,
+              example: 'password123'
+            }
+          }
+        },
+        LoginResponse: {
+          type: 'object',
+          properties: {
+            user: {
+              $ref: '#/components/schemas/User'
+            },
+            token: {
+              type: 'object',
+              properties: {
+                access_token: {
+                  type: 'string',
+                  example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
+                },
+                refresh_token: {
+                  type: 'string',
+                  example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
+                }
+              }
+            }
+          }
+        },
+        User: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'string',
+              example: 'cln6z9p8a0000mg08b6z6b1e2'
+            },
+            username: {
+              type: 'string',
+              example: 'john_doe'
+            },
+            email: {
+              type: 'string',
+              format: 'email',
+              example: 'user@example.com'
+            }
+          }
+        }
       },
-    ],
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT'
+        }
+      }
+    }
   },
-  apis: ['./../routes/*.ts'],
+  apis: ['./src/routes/*.ts', './src/types/*.ts']
 };
 
-export default options;
+export default swaggerOptions;
